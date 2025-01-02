c2ff926c75fca4363124bd9ae49f869268ccfb1a
Salvatore Cirone <s.cirone@reply.it>
2025-01-02 19:58:13 +0100

    :tada: reinit in go

---
+++ main.go
@@ -0,0 +1,11 @@
+/*
+Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+
+*/
+package main
+
+import "github.com/aka-somix/aws-events-gate/cmd"
+
+func main() {
+	cmd.Execute()
+}
--- source/backend/listener-lambda/src/index.ts
+++
@@ -1,5 +0,0 @@
-
-
-export const handler = async (event: unknown): Promise<void> => {
-  console.info("Hello World");
-};
